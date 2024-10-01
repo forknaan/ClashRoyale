fcd586a330a130f7205e27c729a24b7b48ef7f2a
BlackVoid365 <furqan20006@gmail.com>
2024-10-01 21:17:07 +0300

    Initial commit

---
+++ home.py
@@ -0,0 +1,29 @@
+# Furqan Saeed
+# faseed@andrew.cmu.edu
+
+from cmu_graphics import *
+import math
+import string
+import random
+
+
+def bg(app):
+    diamondWH = app.h/11
+    diamondSide = ((diamondWH**2)/2)**0.5 
+
+    drawRect(app.x0, app.y0, app.w, app.h, fill=rgb(19, 96, 153))
+
+    for i in range(11):
+        for j in range(8):
+            drawRect(app.x0 + diamondWH*j, app.y0 + diamondWH*i, diamondSide, diamondSide, 
+                     fill=rgb(19, 83, 139), align='top', rotateAngle=45)
+    
+    drawRect(app.x0, app.y0, app.w, app.height, fill='white', align='top-right')
+    drawRect(app.x0 + app.w, 0, app.width - app.x0 + app.w, app.height, fill='white')
+    drawRect(app.x0, app.y0, app.w, app.h, border='black', fill=None)
+
+def battleButton(app):
+    drawRect(app.x0 + app.w/2, app.h*0.75, app.w*0.4, app.h*0.1, fill=gradient(rgb(255, 208, 83), rgb(255, 188, 44), start='top'),
+             align='center', border='black', borderWidth=app.w*0.001)
+    drawLabel('Battle', app.x0 + app.w/2, app.h*0.75, fill=rgb(255, 255, 204), border='black', borderWidth=app.w*0.001, font='montserrat',
+              size=app.h*0.05)
---
+++ main.py
@@ -0,0 +1,54 @@
+# Furqan Saeed
+# faseed@andrew.cmu.edu
+
+from cmu_graphics import *
+import math
+import string
+import random
+import home
+import time
+
+
+
+
+def getCanvasDetails(app):
+    app.h = app.height
+    app.w = app.h/19.5 * 12
+    app.x0 = app.width/2 - app.w/2
+    app.y0 = 0
+
+
+def onAppStart(app):
+    app.appStart = True
+    getCanvasDetails(app)
+
+
+def redrawAll(app):    
+    if app.appStart:
+        home.bg(app)
+        home.battleButton(app)
+
+
+def onMouseClick(app, x1, y1):
+    pass
+
+def onStep(app):
+    getCanvasDetails(app)
+
+
+def onKeyPress(app, key):
+    pass
+
+def onMouseHover(app, x1, y1):
+    pass
+
+def onMouseRelease(app, x1, y1):
+    pass
+
+
+
+
+
+
+
+runApp()
